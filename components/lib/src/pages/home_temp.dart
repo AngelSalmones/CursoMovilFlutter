f315697f0df480b2557b1c8c2ff51c1a3492de90
AngelSalmones <angel.garcia@cceo.com.mx>
2019-06-28 10:46:01 -0500

    finish day 06-27-19

---
+++ components/lib/src/pages/home_temp.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold (
+      appBar: AppBar(
+        title:  Text('Components Temp'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text ('List Title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text ('List Title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text ('List Title'),
+          )
+        ],
+      ),
+    );
+  }
+}
