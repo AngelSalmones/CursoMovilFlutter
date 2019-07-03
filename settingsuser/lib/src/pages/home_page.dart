97f0e91245b6e01d0eaf635dad11fb24e7fdaaf3
AngelSalmones <angel.garcia@cceo.com.mx>
2019-07-03 15:09:22 -0500

    finish aplication

--- settingsuser/lib/src/pages/home_page.dart
+++ settingsuser/lib/src/pages/home_page.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:settingsuser/src/pages/settings_page.dart';
+import 'package:settingsuser/src/share_prefs/preferences_user.dart';
+import 'package:settingsuser/src/widgets/menu_widget.dart';
 
 class HomePage extends StatelessWidget{
+  final prefs = new PreferencesUser ();
 
   static final String routeName = 'home';
   @override
   Widget build(BuildContext context) {
+    prefs.finalPage = HomePage.routeName;
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text('settings'),
+        backgroundColor:(prefs.secundariColor) ? Colors.teal : Colors.blue ,
       ),
+      drawer: MenuWidget(),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text ('Secundary Color'),
+          Text ('Secundary Color ${prefs.secundariColor}'),
           Divider(),
-          Text ('Gener'),
+          Text ('Gener ${prefs.gener}'),
           Divider(),
-          Text ('User name'),
+          Text ('User name${prefs.userName}'),
           Divider(),
 
 
@@ -24,5 +32,6 @@ class HomePage extends StatelessWidget{
       ),
     );
   }
+
 
 }
--- settingsuser/lib/src/pages/settings_page.dart
+++ settingsuser/lib/src/pages/settings_page.dart
@@ -1,19 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:settingsuser/src/share_prefs/preferences_user.dart';
+import 'package:settingsuser/src/widgets/menu_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
-class SettingsPage extends StatelessWidget{
+class SettingsPage extends StatefulWidget{
   static final String routeName = 'settings';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  bool _secundarycolor;
+  int _gener ;
+  String _name = 'Petter';
+
+  TextEditingController _textController;
+
+  final prefs = new PreferencesUser ();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    prefs.finalPage = SettingsPage.routeName;
+    _gener = prefs.gener;
+    _secundarycolor = prefs.secundariColor;
+    _textController = new TextEditingController(text: prefs.userName);
+  }
+
+  _setSelectdRadio (int value){
+    prefs.gener = value;
+    _gener = value;
+        setState(() { });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
+        backgroundColor:(prefs.secundariColor) ? Colors.teal : Colors.blue ,
       ),
-      body: Center(
-        child: Text('Settings Page'),
-      ),
+      drawer: MenuWidget(),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: Text('Settings',style: TextStyle(fontSize: 45.0,fontWeight: FontWeight.bold),),
+          ),
+          Divider(),
+          SwitchListTile(
+            value: _secundarycolor,
+            title: Text('Secundary color'),
+            onChanged: (value){
+              _secundarycolor = value;
+              setState(() {
+                prefs.secundariColor = value;
+              });
+            },
+          ),
+          RadioListTile(
+            value: 1 ,
+            title: Text('male'),
+            groupValue: _gener,
+            onChanged: _setSelectdRadio,
+          ),
+          RadioListTile(
+            value: 2,
+            title: Text('female'),
+            groupValue: _gener,
+            onChanged: _setSelectdRadio
+          ),
+          Divider(),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                labelText: 'name',
+                helperText: 'name of people'
+              ),
+              onChanged: (value){
+                prefs.userName=value;
+              },
+            ),
+          ),
+        ],
+      )
     );
   }
-
 }
